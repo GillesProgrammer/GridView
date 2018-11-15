dad9b5441c769154341eb1d011c2802f2661d760
GillesProgrammer <dubois.gilles@outlook.fr>
2018-11-15 09:08:07 +0100

    add images + create gridview

--- app/src/main/java/com/example/duboi/gridview/MainActivity.java
+++ app/src/main/java/com/example/duboi/gridview/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.duboi.gridview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
+    GridView grid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        grid = findViewById(R.id.grid);
+
     }
 }
